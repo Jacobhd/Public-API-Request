823f0aee48150374814693a0f9d135dae236d021
Jacobhd <54602167+Jacobhd@users.noreply.github.com>
2021-03-03 09:11:51 -0700

    Update script.js
    this commit will:
    Add insertAfter function to place 'modal-container' div correctly
    Adds correct address data to modal
    Cleans commenting
    
    Meets Expectation Grading

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
    Public API Request
 ======================================== */
 
-/* fetches employees, parsing data to json,
-   returned data displays on screen */
+/* fetch employees, parsing data to json,
+   returned data to display on screen */
 
 function fetchData() {
   fetch('https://randomuser.me/api/?results=12&nat=us,gb,nz,au')
@@ -27,8 +27,8 @@ fetchData();
   }
 
 
-/* Dynamically displays 12 random Employees with basic info,
-   click function shows modal with in-depth information */
+/* Dynamically displays 12 random employees with basic data,
+   click() shows modal with in-depth information */
 
 function appendEmployees(data) {
   const users = data.map((employee, index) => {
@@ -49,6 +49,7 @@ function appendEmployees(data) {
   gallery.insertAdjacentHTML('beforeend', users);
 }
 
+
 /* Clickable card displays employee's data in closable modal */
 
 function createModal(employee, data, index) {
@@ -65,12 +66,14 @@ function createModal(employee, data, index) {
       <p class="modal-text cap">${employee.location.city}</p>
       <hr>
       <p class="modal-text">Cell: ${employee.cell}</p>
-      <p class="modal-text cap">Address: ${employee.location.street}, ${employee.location.city}, ${employee.location.state}, ${employee.location.postcode}</p>
+      <p class="modal-text cap">Address: ${employee.location.street.number}, ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state}, ${employee.location.postcode}</p>
       <p class="modal-text">Birthday: ${dob}</p>
     </div>
   </div>`
+
   div.insertAdjacentHTML('beforeend', html);
-  document.querySelector('body').appendChild(div);
+  insertAfter(div, gallery);
+
 
   const closeButton = document.getElementById('modal-close-btn');
   closeButton.addEventListener('click', e => {
@@ -79,10 +82,17 @@ function createModal(employee, data, index) {
 }
 
 
+/* When modal div is created, places correctly in DOM */
+
+function insertAfter(div, gallery) {
+  gallery.parentNode.insertBefore(div, gallery.nextSibling);
+}
+
+
 /* Displays modal when Employee card is clicked */
 
 function click(employee, data, index) {
-  setTimeout(event => {
+  setTimeout(e => {
     const person = document.getElementById(`${employee.name.first}${employee.name.last}`);
     person.addEventListener('click', e => createModal(employee, data, index));
   }, 100)
@@ -98,20 +108,3 @@ function getFormattedDOB(date) {
   let year = birthday.getFullYear();
   return month + "/" + day + "/" + year;
 }
-
-
-
-/*  Comments   *//*
-
-Displays 12 random employees to page
-correct data in body Photo / Name / Email / city
-correct data in modal photo / name / email / city
-------------------seperation -----------------------
-cell / address ( city / nationality / postal ) / birthday
-closable modal
-
-
-error pulling address in Modal
-placing "modal-container" div at end of body element (after script)
-
-*/
